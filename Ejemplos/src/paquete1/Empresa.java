f90060b2a9ea0f096f88c44f6d92cbcebee8cb1a
carolaljime21 <carolalvaradojime@gmail.com>
2022-06-10 11:24:53 -0500

    clase01B2

--- Ejemplos/src/paquete1/Empresa.java
+++ Ejemplos/src/paquete1/Empresa.java
@@ -10,23 +10,54 @@ package paquete1;
  * @author reroes
  */
 public class Empresa {
+
     private String nombre;
-    private Edificio [] edificios;
+    private Edificio[] edificios;
     private double costoBienesInmuebles;
-    
-    public void establecerNombre(String m){
+
+    public void establecerNombre(String m) {
         nombre = m;
     }
-    
-    public void establecerEdificios(Edificio[] m){
+
+    public void establecerEdificios(Edificio[] m) {
         edificios = m;
     }
-    
-    public String obtenerNombre(){
+
+    public void establecerCostoBienesInmuebles() {
+        double costo = 0;
+        for (int i = 0; i < obtenerEdificios().length; i++) {
+            costo = costo + obtenerEdificios()[i].obtenerCosto();
+        }
+        costoBienesInmuebles = costo;
+    }
+
+    public String obtenerNombre() {
         return nombre;
     }
-    
-    public Edificio[] obtenerEdificios(){
+
+    public Edificio[] obtenerEdificios() {
         return edificios;
     }
+
+    public double obtenerCostoBienesInmuebles() {
+        return costoBienesInmuebles;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("%s\n Lista de Edificios\n",
+                obtenerNombre());
+
+        for (int i = 0; i < obtenerEdificios().length; i++) {
+            cadena = String.format("%s %d. %s (%.2f)\n", cadena, (i + 1),
+                    obtenerEdificios()[i].obtenerNombre().toUpperCase(),
+                    obtenerEdificios()[i].obtenerCosto());
+        }
+        
+        cadena = String.format("%s Total de inmuebles: %.2f", 
+                cadena, obtenerCostoBienesInmuebles());
+
+        return cadena;
+    }
+
 }
--- Ejemplos/src/paquete2/Ejecutor.java
+++ Ejemplos/src/paquete2/Ejecutor.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package paquete2;
-
+import paquete1.Edificio;
+import paquete1.Empresa;
 /**
  *
  * @author reroes
@@ -13,8 +14,40 @@ public class Ejecutor {
     public static void main(String[] args) {
         
         // Crear una arreglo de 4 elementos de tipo Edificio
+        Edificio edf1 = new Edificio("edificio central");
+        edf1.establecerCostos(10000);
+//        System.out.println(edf1);
         
+        Edificio edf2 = new Edificio("edificio sur");
+        edf2.establecerCostos(20000);
+        
+        Edificio edf3 = new Edificio("edificio norte");
+        edf3.establecerCostos(30000);
+        
+        Edificio edf4 = new Edificio("edificio secundario");
+        edf4.establecerCostos(20000);
+        
+        Edificio[] edificios = new Edificio[4];
+        edificios[0] = edf1;
+        edificios[1] = edf2;
+        edificios[2] = edf3;
+        edificios[3] = edf4;
+        
+//        for (int i = 0; i < edificios.length; i++) {
+//            System.out.printf("Edificio %s\n%s\n", (i+1), edificios[i]);
+//        }
+        
+        Empresa emp1 = new Empresa();
+        emp1.establecerNombre("Empresa de hojas");
+        emp1.establecerEdificios(edificios);
+        emp1.establecerCostoBienesInmuebles();
+        emp1.obtenerEdificios();
+        emp1.obtenerCostoBienesInmuebles();
+        
+        //System.out.printf("%.2f\n", emp1.obtenerCostoBienesInmuebles());
         // Crear un objeto de tipo Empresa:
+        System.out.printf("%s\n", emp1);
+        
         //  Nombre: Empresa de Hojas
         //  Lista de edificio: edf1, edf2, edf3, edf4
         
